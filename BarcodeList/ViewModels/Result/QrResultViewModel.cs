e12c8300983b1d1742354ed4fc301a59e149133a
tashiro shingo <tashihub197@gmail.com>
2026-07-01 22:48:54 +0900

    QRコードのフォルダ作成と保存作成

--- BarcodeList/ViewModels/Result/QrResultViewModel.cs
+++ BarcodeList/ViewModels/Result/QrResultViewModel.cs
@@ -1,4 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BarcodeList.Models;
+using BarcodeList.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using ZXing.Net.Maui;
 
 namespace BarcodeList.ViewModels.Result
 {
@@ -12,6 +17,97 @@ namespace BarcodeList.ViewModels.Result
             if (query.TryGetValue("qrValue", out var value))
             {
                 QrValue = (string)value;
+            }
+        }
+
+
+        [ObservableProperty]
+        private BarcodeFormat barcodeFormat = BarcodeFormat.Code128;
+
+        [ObservableProperty]
+        public string displayValue = "";
+
+        [ObservableProperty]
+        private BarcodeFolder? selectedFolder;
+
+        [ObservableProperty]
+        private string name = "";
+
+        [ObservableProperty]
+        private SavedBarcode? savedBarcode;
+
+        [ObservableProperty]
+        private ObservableCollection<BarcodeFolder> folders = new ObservableCollection<BarcodeFolder>();
+
+        private readonly FolderService _folderService;
+        private readonly BarcodeResultService _barcodeResultService;
+
+        public QrResultViewModel(FolderService folderService, BarcodeResultService barcodeResultService)
+        {
+            _folderService = folderService;
+            _barcodeResultService = barcodeResultService;
+        }
+
+        /// <summary>
+        /// 初期化処理。フォルダ一覧を取得してViewModelに設定する
+        /// </summary>
+        /// <returns></returns>
+        internal async Task InitializeAsync()
+        {
+            try
+            {
+                Folders = await _barcodeResultService.LoadFoldersAsync();
+                Name = Folders.Count > 0 ? Folders[0].Name : "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while initializing: {ex.Message}");
+            }
+
+        }
+
+        [RelayCommand]
+        private async Task CreateFolder()
+        {
+            var newFolder = await _folderService.CreateFolderAsync();
+            if (newFolder == null)
+            {
+                Console.WriteLine("フォルダの作成がキャンセルされました。");
+                return;
+            }
+            Folders.Add(newFolder);
+            SelectedFolder = newFolder;
+        }
+
+        /// <summary>
+        /// フォルダ込みの履歴に保存する。★通常履歴にはCrearteで登録する
+        /// </summary>
+        /// <returns></returns>
+        [RelayCommand]
+        private async Task Save()
+        {
+            if (SelectedFolder == null)
+            {
+                await Shell.Current.DisplayAlertAsync("フォルダ未選択", "保存するフォルダを選択してください。", "OK");
+                return;
+            }
+
+            SavedBarcode = new SavedBarcode
+            {
+                BarcodeValue = QrValue,
+                BarcodeType = BarcodeFormat.QrCode.ToString(),
+                FolderId = SelectedFolder?.Id ?? 0,
+                CreatedAt = DateTime.Now,
+            };
+            bool success = await _barcodeResultService.SaveToFolderAsync(QrValue, BarcodeFormat.QrCode, SelectedFolder);
+            if (success)
+            {
+                await Shell.Current.DisplayAlertAsync("保存完了", $"バーコードをフォルダ「{SelectedFolder.Name}」に保存しました。", "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlertAsync("保存失敗", "バーコードの保存に失敗しました。", "OK");
+                Console.WriteLine("Failed to save barcode.");
             }
         }
     }
--- BarcodeList/Views/Result/QrResultView.xaml.cs
+++ BarcodeList/Views/Result/QrResultView.xaml.cs
@@ -1,13 +1,27 @@
+using BarcodeList.Services;
 using BarcodeList.ViewModels.Result;
 
 namespace BarcodeList.Views.Result;
 
 public partial class QrResultView : ContentPage
 {
-	private readonly QrResultViewModel _viewModel = new();
-    public QrResultView()
+    private readonly QrResultViewModel _viewModel;
+    public QrResultView(FolderService folderService, BarcodeResultService barcodeResultService)
 	{
 		InitializeComponent();
+        _viewModel = new QrResultViewModel(folderService, barcodeResultService);
         BindingContext = _viewModel;
+    }
+
+    bool _isInitialized = false;
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        // フォルダ一覧を取得してViewModelに設定
+        if (!_isInitialized)
+        {
+            await _viewModel.InitializeAsync();
+            _isInitialized = true;
+        }
     }
 }
